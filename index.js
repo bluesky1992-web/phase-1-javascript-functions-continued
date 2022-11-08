beef2d9feeae980cf8dccfcb154b2cba3b875d96
Ali ibrahim jasim <ali.ibrahim.jasim@gmail.com>
2022-11-08 13:43:32 +0300

    Completed assignment.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // code your solution here
+
+function saturdayFun(arg = "roller-skate") {
+  return `This Saturday, I want to ${arg}!`
+}
+console.log(saturdayFun( "bathe my dog"))
+
+const mondayWork = function(val = "go to the office") {
+    return `This Monday, I will ${val}.`
+}
+console.log(mondayWork())
+
+function wrapAdjective(string) {
+    return function(arg = "special") {
+       return     `You are ${string}${arg}${string}!`
+    }
+
+}
+
+let result = wrapAdjective("!!!")("a dedicated programmer")
+console.log(result)
